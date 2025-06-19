532dd62f8dda700c23b5d2b3279c6f683150f83a
Vinicius <vinicius.oliveira100307@gmail.com>
2025-06-18 22:56:07 -0300

    Desktop 50%

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        lobster: ['"Lobster Two"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'], // ← 'sans-serif' como fallback
+      },
+    },
+  },
+}
